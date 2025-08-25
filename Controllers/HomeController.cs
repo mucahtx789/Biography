bf74430ee45b3237a9f7cfde9c6e4e33c9ed6628
mucahtx789 <mucahtx789@gmail.com>
2025-08-25 17:37:45 +0300

    2508

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Biography.Controllers
             var model = new BiographyM
             {
                 FullName = "Mücahit Demir",
-                PhotoUrl = "/images/profile.jpg",
+                PhotoUrl = "images\\profile.jpg",
                 About = "Yazýlým geliþtirici olarak eðitim ve hobilerim hakkýnda kýsa bilgi..."
             };
             return View(model);
@@ -19,18 +19,80 @@ namespace Biography.Controllers
 
         public IActionResult Projects()
         {
-            var projects = new List<(string Name, string Link, string Description)>
+            var projects = new List<ProjectModel>
+    {
+                new ProjectModel
         {
-            ("SellPoint", "https://github.com/mucahtx789/SellPoint", "Alýþveriþ sitesi projesi"),
-            ("Anket Uygulamasý", "https://github.com/mucahtx789/AnketUygulamasi", "Vue.js ile yapýlmýþ anket uygulamasý")
-            // Ýstediðin kadar ekleyebilirsin
-        };
+            Name = "Anket Uygulamasý",
+            Link = "https://github.com/mucahtx789/ankett",
+            Description = "Vue.js ile yapýlmýþ anket uygulamasý"
+        },
+                new ProjectModel
+        {
+            Name = "Hastane Randevu Sistemi",
+            Link = "https://github.com/mucahtx789/Randevu",
+            Description = "Kullanýcýlar için randevu almayý ve yönetmeyi saðlayan uygulama."
+        },
+        new ProjectModel
+        {
+            Name = "SellPoint",
+            Link = "https://github.com/mucahtx789/SellPoint",
+            Description = "Alýþveriþ sitesi projesi"
+        },
+        new ProjectModel
+        {
+            Name = "Cookie Authentication - .NET 6",
+            Link = "https://github.com/mucahtx789/ASPNET-MVC-NET6--Cookie-Auth-2",
+            Description = "ASP.NET MVC üzerinde cookie tabanlý oturum yönetimi örneði."
+        },
+        new ProjectModel
+        {
+            Name = "Cookie Authentication - .NET 6",
+            Link = "https://github.com/mucahtx789/ASPNET-MVC-NET6--Cookie-Auth-2",
+            Description = "ASP.NET MVC üzerinde cookie tabanlý oturum yönetimi örneði."
+        },
+        new ProjectModel
+        {
+            Name = "Emlak",
+            Link = "https://github.com/mucahtx789/Emlak",
+            Description = "ASP.NET Core ve Entity Framework ile geliþtirilen emlak ilan yönetim sistemi.."
+        }
+        ,
+        new ProjectModel
+        {
+            Name = "Emlak",
+            Link = "https://github.com/mucahtx789/Emlak",
+            Description = "ASP.NET Core ve Entity Framework ile geliþtirilen emlak ilan yönetim sistemi."
+        }
+        ,
+        new ProjectModel
+        {
+            Name = "Sosyal Medya",
+            Link = "https://github.com/mucahtx789/sosyalMedya",
+            Description = "Kullanýcý, gönderi, yorum ve beðeni özelliklerine sahip bir sosyal medya platformu."
+        }
+        ,
+        new ProjectModel
+        {
+            Name = "Görev Takip",
+            Link = "https://github.com/mucahtx789/TaskTracker",
+            Description = "Kullanýcý görev oluþturma ve takip uygulamasý"
+        }
+        ,
+        new ProjectModel
+        {
+            Name = "Emlak",
+            Link = "https://github.com/mucahtx789/Emlak",
+            Description = "ASP.NET Core ve Entity Framework ile geliþtirilen emlak ilan yönetim sistemi.."
+        }
+    };
+
             return View(projects);
         }
 
         public IActionResult Contact()
         {
             return View();
-        }
+        } 
     }
 }
---
+++ Models/ProjectModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Biography.Models
+{
+    public class ProjectModel
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+    }
+}
