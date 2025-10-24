49fefefccf7d6638939b80e686d382c4e77fb922
mucahtx789 <mucahtx789@gmail.com>
2025-10-24 21:30:24 +0300

    24102025

---
+++ Controllers/SitemapController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Biography.Controllers
+{
+    public class SitemapController : Controller
+    {
+        
+        [Route("sitemap.xml")]
+        public IActionResult Index()
+        {
+            Response.ContentType = "application/xml";
+
+            // Sitemap namespace
+            XNamespace ns = "https://www.sitemaps.org/schemas/sitemap/0.9";
+
+            // URL seti
+            var urlset = new XElement(ns + "urlset",
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "http://mucahitd.com.tr/"),
+                    new XElement(ns + "lastmod", DateTime.UtcNow.ToString("yyyy-MM-dd")),
+                    new XElement(ns + "changefreq", "daily"),
+                    new XElement(ns + "priority", "1.0")
+                )
+            );
+
+            var sitemap = new XDocument(urlset);
+
+            return Content(sitemap.ToString(), "application/xml");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace Biography
 {
     public class Program
@@ -15,17 +19,15 @@ namespace Biography
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
+            // Default route
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
